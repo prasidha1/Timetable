407ea442835c95bfbbcaa9eea27c24ef95243188
prasidha1 <32433336+prasidha1@users.noreply.github.com>
2022-02-16 11:58:23 +0530

    Delete user/student directory

--- user/student/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from student.models import Activity,Program, Centre, Professor
-
-
-admin.site.register(Program)
-admin.site.register(Centre)
-admin.site.register(Professor)
-admin.site.register(Activity)
